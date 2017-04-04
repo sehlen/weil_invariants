903f637e939481c5ed34f66370fb6aca37f5739f
Stephan Ehlen <stephan.j.ehlen@gmail.com>
2017-04-04 15:26:44 +0200

    change all.py after exporting weil invariants code

--- all.py
+++ all.py
@@ -1,3 +1,1 @@
-from vector_complex_dense import Vector_complex_dense
-from finite_quadratic_module import FiniteQuadraticModule,FiniteQuadraticModule
-from weil_module import WeilModule,sigma_cocycle,kubota_cocycle
+from weil_invariants import invariants, cython_invariants
